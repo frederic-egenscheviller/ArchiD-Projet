a2ee7521f91cea038f8002745a5d49c3d5726aeb
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2024-01-18 20:45:02 +0100

    feat: add research bar

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DropdownModule } from 'primeng/dropdown';
 
 import { AppComponent } from './app.component';
 import { ResearchBarComponent } from './research-bar/research-bar.component';
 import {ThemeService} from "../theme/service";
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MultiSelectModule} from "primeng/multiselect";
+import {AirportSelectorComponent} from "./research-bar/airport-selector/airport-selector.component";
+import {SensorSelectorComponent} from "./research-bar/sensor-selector/sensor-selector.component";
 
 @NgModule({
   declarations: [
@@ -11,7 +17,13 @@ import {ThemeService} from "../theme/service";
     ResearchBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DropdownModule,
+    MultiSelectModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AirportSelectorComponent,
+    SensorSelectorComponent
   ],
   providers: [ThemeService],
   bootstrap: [AppComponent]
